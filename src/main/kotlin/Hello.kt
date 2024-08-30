b80724a8df2c11fea690e474dde84f3e28771dfa
ombatii <brianogeto896@gmail.com>
2024-08-31 00:59:53 +0300

    Second commit

---
+++ src/main/kotlin/Hello.kt
@@ -0,0 +1,5 @@
+fun main() {
+    print("Please enter your name: ")
+    val name = readLine()
+    println("Hello, $name!")
+}
---
+++ src/main/kotlin/MailingAddress.kt
@@ -0,0 +1,18 @@
+fun main() {
+    /**
+     * This program displays your name and complete mailing address formatted in
+     * the manner that you would usually see it on the outside of an envelope.
+     * The program does not need to read any input from the user.
+     */
+    val name = "Brian Ombati"
+    val streetAddress = "1234 Winner Street"
+    val city = "Nairobi"
+    val state = "Kenya"
+    val postalCode = "56100"
+
+    println("Name: $name")
+    println("Street Address: $streetAddress")
+    println("City: $city")
+    println("State: $state")
+    println("Postal Code: $postalCode")
+}
